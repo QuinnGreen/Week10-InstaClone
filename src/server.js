2e488870bc3f0c658331e6c4b7e1973d5d6bb878
quinn <williammsullivansabre@gmail.com>
2024-02-28 10:53:30 +0000

    chore: basic server, skeleton and model

---
+++ src/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const User = require("./users/model");
+
+const userRouter = require("./users/routes");
+
+const port = process.env.PORT || 5001;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(userRouter);
+
+const syncTables = () => {
+  User.sync();
+};
+
+app.listen(port, () => {
+  syncTables();
+  console.log(`Server is listening on port ${port}`);
+});
